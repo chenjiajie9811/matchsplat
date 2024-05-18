666b3a56aba3839a3880502db13a3fa19964a87e
Jiajie Chen <jiajiechen11@gmail.com>
2024-05-18 14:13:29 +0200

    Fixed reprojection bug

--- src/loss/loss_corres.py
+++ src/loss/loss_corres.py
@@ -18,6 +18,9 @@ import matplotlib.cm as cm
 from plyfile import PlyData, PlyElement
 from torchvision.utils import save_image
 from PIL import Image
+from einops.einops import rearrange
+
+from ..geometry.projection import get_world_rays, sample_image_grid
 
 def save_points_ply(points, path):
     vertex = np.array([(points[i][0], points[i][1], points[i][2]) 
@@ -60,6 +63,22 @@ def iproj_full_img(depth, intr):
     # pts = torch.stack([pts, i], dim=-1)
     return pts.reshape(-1, 3)
 
+def iproj_full_img_(depth: Tensor, intr: Tensor, extr: Tensor):
+    ht, wd = depth.shape[-2:]
+    depth = depth.reshape(1, -1) 
+    xy_ray, _ = sample_image_grid(((ht, wd)), depth.device)
+    xy_ray = xy_ray.reshape(1, -1, 2)
+    origins, directions = get_world_rays(xy_ray, extr, intr)
+    return origins + directions * depth[..., None]
+
+def iproj_full_img_identity_depth(depth: Tensor, intr: Tensor, extr: Tensor):
+    ht, wd = depth.shape[-2:]
+    depth = depth.reshape(1, -1) 
+    xy_ray, _ = sample_image_grid(((ht, wd)), depth.device)
+    xy_ray = xy_ray.reshape(1, -1, 2)
+    origins, directions = get_world_rays(xy_ray, extr, intr)
+    return origins + directions #* depth[..., None]
+
 # @TODO: Move these geometry related functions to /geometry/projection or check whether we can reuse
 def to_homogeneous(points: Union[torch.Tensor, np.ndarray]):
     """Convert N-dimensional points to homogeneous coordinates.
@@ -100,6 +119,10 @@ def iproj_kpts(
     return kpts_3d_0
 
 def pc_transform(pts3d_i: Float[Tensor, "N 3"], Tij: Float[Tensor, "4 4"])-> Float[Tensor, "N 3"]:
+    # Tij = Tij.transpose(-1, -2)
+    # pts3d_i = pts3d_i @ Tij[:3, :3]
+    # pts3d_i = pts3d_i  + Tij[:3, -1:].transpose(0,1).repeat(pts3d_i.size()[0],1)
+    # return pts3d_i
     return from_homogeneous(to_homogeneous(pts3d_i) @ Tij.transpose(-1, -2))
 
 def proj(pts3d: Float[Tensor, "N 3"], intr: Float[Tensor, "3 3"])->Float[Tensor, "N 2"]:
@@ -173,6 +196,7 @@ class LossCorres(Loss[LossCorresCfg, LossCorresCfgWrapper]):
         global_step: int,
     ) -> Float[Tensor, ""]:
         depths = batch["context"]["rendered_depth"]
+        depths_gaussians = batch["context"]["est_depth"].clone().squeeze()
         mbids = batch["mbids"]
 
         b, v, h, w = depths.shape
@@ -189,9 +213,10 @@ class LossCorres(Loss[LossCorresCfg, LossCorresCfgWrapper]):
         for i in range(b):
             b_mask = mbids == i
             mkpts0 = batch["mkpts0"][b_mask]
-            mkpts1 = batch["mkpts0"][b_mask]
+            mkpts1 = batch["mkpts1"][b_mask]
             mconf = batch["mconf"][b_mask]
 
+            # print ("extrinsics shape", batch["context"]["extrinsics"].shape)
             extr0 = batch["context"]["extrinsics"][i, 0].clone().detach()
             extr1 = batch["context"]["extrinsics"][i, 1].clone().detach()
 
@@ -204,72 +229,152 @@ class LossCorres(Loss[LossCorresCfg, LossCorresCfgWrapper]):
             intr1[1, :] *= h
 
             img0 = batch['context']['image'][i, 0]
-            img1 = batch['context']['image'][i, 0]
+            img1 = batch['context']['image'][i, 1]
             img0_numpy = img0.clone().detach().cpu().numpy().transpose(1, 2, 0)
             img1_numpy = img1.clone().detach().cpu().numpy().transpose(1, 2, 0)
 
-            depth0 = depths[i, 0].view(h, w)
-            depth1 = depths[i, 1].view(h, w)
+            depth0 = depths[i, 0].clone().view(h, w)
+            depth1 = depths[i, 1].clone().view(h, w)
 
-            depth0 = 1. / depth0
-            depth1 = 1. / depth1
+            # depth0 = 1. / depth0
+            # depth1 = 1. / depth1
 
             if self.cfg.use_corres_repro:
                 T01 = torch.inverse(extr1) @ extr0
                 T10 = torch.inverse(extr0) @ extr1
+
                 # Reprojection error with the estimated depth from 0->1 and 1->0
                 x0_1, X0 = projective_transform(T01, depth0, intr0, intr1, mkpts0, True)
                 x1_0, X1 = projective_transform(T10, depth1, intr1, intr0, mkpts1, True)
 
                 
-
+                mask_0 = (x0_1[:, 0] >= 0) & (x0_1[:, 0] < w) & (x0_1[:, 1] >= 0) & (x0_1[:, 1] < h)
+                mask_1 = (x1_0[:, 0] >= 0) & (x1_0[:, 0] < w) & (x1_0[:, 1] >= 0) & (x1_0[:, 1] < h)
+                
                 # @TODO Add the mask for filtering out those with large reprojection error 
-                loss0_1 = torch.sum(mconf * torch.norm(x0_1 - mkpts1, dim=-1))
-                loss1_0 = torch.sum(mconf * torch.norm(x1_0 - mkpts0, dim=-1))
-
+                loss0_1 = torch.sum(mconf[mask_0] * torch.norm(x0_1[mask_0] - mkpts1[mask_0], dim=-1))
+                loss1_0 = torch.sum(mconf[mask_1] * torch.norm(x1_0[mask_1] - mkpts0[mask_1], dim=-1))
                 loss_repro += (loss0_1 + loss1_0) / 2.
 
-                if global_step % 10 == 0:
-
+                if global_step % 50 == 0:
+                    x0_1, mkpts0 = x0_1[mask_0], mkpts0[mask_0]
+                    x1_0, mkpts1 = x1_0[mask_1], mkpts1[mask_1]
                     pts3d_0 = iproj_full_img(depth0, intr0)
                     perm = torch.randperm(pts3d_0.size(0))
                     idx = perm[:1000]
                     pts3d_0 = pts3d_0[idx]
-                    
-                    save_points_ply(pts3d_0.clone().detach().cpu().numpy(), f"outputs/tmp/iproj_full_batch{i}_step{global_step}.ply")
-                    plt.figure()
-                    plt.imshow(img0_numpy)
-                    plt.savefig(f"outputs/tmp/img0_batch{i}_step{global_step}.png")
-                    plt.figure()
-                    plt.imshow(img1_numpy)
-                    plt.savefig(f"outputs/tmp/img1_batch{i}_step{global_step}.png")
-
-
-                    
-                    depth0_viz = viz_depth_tensor(1. / depth0.clone().detach().cpu(), True)
-                    depth1_viz = viz_depth_tensor(1. / depth1.clone().detach().cpu(), True)
-                    depth0_viz = Image.fromarray(depth0_viz)
-                    depth1_viz = Image.fromarray(depth1_viz)
-                    depth0_viz.save(f"outputs/tmp/depth0_batch{i}_step{global_step}.png")
-                    depth1_viz.save(f"outputs/tmp/depth1_batch{i}_step{global_step}.png")
-
-                    # save_image(depth0_viz.long(), f"outputs/tmp/depth0_batch{i}_step{global_step}.png")
-                    # save_image(depth1_viz.long(), f"outputs/tmp/depth1_batch{i}_step{global_step}.png")
-
+                   
                     plt.figure()
                     fig, ax = plt.subplots(nrows=1, ncols=2)
                     ax[0].imshow(img0_numpy)
                     ax[0].scatter(x1_0[:, 0].clone().detach().cpu().numpy(), x1_0[:, 1].clone().detach().cpu().numpy(), s=1, c='red')
                     ax[0].scatter(mkpts0[:, 0].clone().detach().cpu().numpy(), mkpts0[:, 1].clone().detach().cpu().numpy(), s=1, c='blue')
+                    
                     ax[1].imshow(img1_numpy)
                     ax[1].scatter(x0_1[:, 0].clone().detach().cpu().numpy(), x0_1[:, 1].clone().detach().cpu().numpy(), s=1, c='red')
                     ax[1].scatter(mkpts1[:, 0].clone().detach().cpu().numpy(), mkpts1[:, 1].clone().detach().cpu().numpy(), s=1, c='blue')
                     plt.savefig(f"outputs/tmp/reproj_120_021_batch{i}_step{global_step}.png")
+                    # input()
 
-                    save_points_ply(X0.clone().detach().cpu().numpy(), f"outputs/tmp/iproj_kpts0_batch{i}_step{global_step}.ply")
-                    save_points_ply(X1.clone().detach().cpu().numpy(), f"outputs/tmp/iproj_kpts1_batch{i}_step{global_step}.ply")
+                # if global_step % 10 == 0:
 
-                    input()
+                #     pts3d_0 = iproj_full_img_(depth0, intr0.unsqueeze(0), extr0.inverse().unsqueeze(0)).squeeze(0)
+                #     perm = torch.randperm(pts3d_0.size(0))
+                #     idx = perm[:1000]
+                #     pts3d_0 = pts3d_0[idx]
+
+                #     pts3d_1 = iproj_full_img_(depth1, intr1.unsqueeze(0), extr1.inverse().unsqueeze(0)).squeeze(0)
+                #     perm = torch.randperm(pts3d_1.size(0))
+                #     idx = perm[:1000]
+                #     pts3d_1 = pts3d_1[idx]
+
+                #     pts3d_0_identity = iproj_full_img_identity_depth(depth0, intr0.unsqueeze(0), extr0.unsqueeze(0)).squeeze(0)
+                #     perm = torch.randperm(pts3d_0_identity.size(0))
+                #     idx = perm[:1000]
+                #     pts3d_0_identity = pts3d_0_identity[idx]
+
+                #     pts3d_1_identity = iproj_full_img_identity_depth(depth1, intr1.unsqueeze(0), extr1.unsqueeze(0)).squeeze(0)
+                #     perm = torch.randperm(pts3d_1_identity.size(0))
+                #     idx = perm[:1000]
+                #     pts3d_1_identity = pts3d_1_identity[idx]
+
+                #     # transform both identity to coord 1
+                #     pts3d_0_identity_0 = pts3d_0_identity
+                #     save_points_ply(pts3d_0_identity_0, f"outputs/tmp/iproj_identity_0.ply")
+                #     pts3d_1_identity_0 = pc_transform(pts3d_1_identity, extr0 @ extr1.inverse())
+                #     save_points_ply(pts3d_1_identity_0, f"outputs/tmp/iproj_identity_1.ply")
+        
+                #     import open3d as o3d
+                #     identity0 = o3d.io.read_point_cloud(f"outputs/tmp/iproj_identity_0.ply")
+                #     identity1 = o3d.io.read_point_cloud(f"outputs/tmp/iproj_identity_1.ply")
+
+                #     identity0.paint_uniform_color([1, 0, 0]) 
+                #     identity1.paint_uniform_color([0, 0, 1])
+
+                #     identity_combined = identity0 + identity1
+                #     o3d.io.write_point_cloud(f"outputs/tmp/iproj_identity_combined.ply", identity_combined)
+
+
+                #     pts3d_0_0 = pts3d_0
+                #     save_points_ply(pts3d_0_0, f"outputs/tmp/iproj_0.ply")
+                #     pts3d_1_0 = pc_transform(pts3d_1, extr0 @ extr1.inverse())
+                #     save_points_ply(pts3d_1_0, f"outputs/tmp/iproj_1.ply")
+
+                #     i0 = o3d.io.read_point_cloud(f"outputs/tmp/iproj_0.ply")
+                #     i1 = o3d.io.read_point_cloud(f"outputs/tmp/iproj_1.ply")
+
+                #     i0.paint_uniform_color([1, 0, 0])
+                #     i1.paint_uniform_color([0, 0, 1])
+
+                #     i_combined = i0 + i1
+                #     o3d.io.write_point_cloud(f"outputs/tmp/iproj_combined.ply", i_combined)
+
+
+                #     # pts3d_0 = iproj_full_img(depth0, intr0)
+                #     # perm = torch.randperm(pts3d_0.size(0))
+                #     # idx = perm[:1000]
+                #     # pts3d_0 = pts3d_0[idx]
+
+                #     # pts3d_1 = iproj_full_img(depth1, intr1)
+                #     # perm = torch.randperm(pts3d_1.size(0))
+                #     # idx = perm[:1000]
+                #     # pts3d_1 = pts3d_1[idx]
+
+                #     pts3d_0_1 = pc_transform(pts3d_0, T10)
+                #     pts3d_both_in_1 = torch.cat([pts3d_0_1, pts3d_1], dim=-0).detach().cpu().numpy()
+
+                #     pts3d_both_in_world = torch.cat([pts3d_0, pts3d_1, pts3d_0_identity, pts3d_1_identity], dim=-0).detach().cpu().numpy()
+
+                #     print (pts3d_0.shape)
+                #     print (pts3d_1.shape)
+                #     print (pts3d_both_in_world.shape)
+                #     save_points_ply(pts3d_both_in_world, f"outputs/tmp/iproj_full_in1_batch{i}_step{global_step}.ply")
+                #     save_points_ply(pts3d_0.clone().detach().cpu().numpy(), f"outputs/tmp/iproj_full0_batch{i}_step{global_step}.ply")
+                #     save_points_ply(pts3d_1.clone().detach().cpu().numpy(), f"outputs/tmp/iproj_full1_batch{i}_step{global_step}.ply")
+                #     plt.figure()
+                #     plt.imshow(img0_numpy)
+                #     plt.savefig(f"outputs/tmp/img0_batch{i}_step{global_step}.png")
+                #     plt.figure()
+                #     plt.imshow(img1_numpy)
+                #     plt.savefig(f"outputs/tmp/img1_batch{i}_step{global_step}.png")
+
+                #     # save_image(depth0_viz.long(), f"outputs/tmp/depth0_batch{i}_step{global_step}.png")
+                #     # save_image(depth1_viz.long(), f"outputs/tmp/depth1_batch{i}_step{global_step}.png")
+
+                #     plt.figure()
+                #     fig, ax = plt.subplots(nrows=1, ncols=2)
+                #     ax[0].imshow(img0_numpy)
+                #     ax[0].scatter(x1_0[:, 0].clone().detach().cpu().numpy(), x1_0[:, 1].clone().detach().cpu().numpy(), s=1, c='red')
+                #     ax[0].scatter(mkpts0[:, 0].clone().detach().cpu().numpy(), mkpts0[:, 1].clone().detach().cpu().numpy(), s=1, c='blue')
+                #     ax[1].imshow(img1_numpy)
+                #     ax[1].scatter(x0_1[:, 0].clone().detach().cpu().numpy(), x0_1[:, 1].clone().detach().cpu().numpy(), s=1, c='red')
+                #     ax[1].scatter(mkpts1[:, 0].clone().detach().cpu().numpy(), mkpts1[:, 1].clone().detach().cpu().numpy(), s=1, c='blue')
+                #     plt.savefig(f"outputs/tmp/reproj_120_021_batch{i}_step{global_step}.png")
+
+                #     save_points_ply(X0.clone().detach().cpu().numpy(), f"outputs/tmp/iproj_kpts0_batch{i}_step{global_step}.ply")
+                #     save_points_ply(X1.clone().detach().cpu().numpy(), f"outputs/tmp/iproj_kpts1_batch{i}_step{global_step}.ply")
+
+                #     input()
 
 
             if self.cfg.use_corres_depth:
@@ -298,7 +403,7 @@ class LossCorres(Loss[LossCorresCfg, LossCorresCfgWrapper]):
                 kpts_est_depth_0 = depth0[mkpts0[:, 1].long().squeeze(), mkpts0[:, 0].long().squeeze()]
                 kpts_est_depth_1 = depth0[mkpts1[:, 1].long().squeeze(), mkpts1[:, 0].long().squeeze()]
 
-                # Compute loss by comparing the ration with 1
+                # Compute loss by comparing the ratio with 1
                 loss_d_0 = torch.sum(torch.abs((kpts_est_depth_0 / (kpts_gt_depth_0 + 1e-6)) - 1.))
                 loss_d_1 = torch.sum(torch.abs((kpts_est_depth_1 / (kpts_gt_depth_1 + 1e-6)) - 1.))
 
