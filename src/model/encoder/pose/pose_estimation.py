c75a6a02e7157b0274245eabb5068072aca7a56d
Jiajie Chen <jiajiechen11@gmail.com>
2024-05-31 18:52:05 +0200

    Added weighted Pnp

--- src/model/encoder/pose/pose_estimation.py
+++ src/model/encoder/pose/pose_estimation.py
@@ -1,10 +1,16 @@
 import cv2
 from einops import rearrange
+from typing import Optional, Tuple
 import torch
 import numpy as np
 from kornia.geometry.epipolar import normalize_points, normalize_transformation, motion_from_essential, \
     motion_from_essential_choose_solution, triangulate_points, symmetrical_epipolar_distance
 from kornia.geometry.epipolar.projection import depth_from_point
+from kornia.geometry.conversions import convert_points_to_homogeneous
+from kornia.geometry.linalg import transform_points
+from kornia.utils import eye_like
+from kornia.utils.helpers import _torch_linalg_svdvals
+from kornia.core import arange, ones_like, where, zeros
 
 from ..loftr.loftr import LoFTR
 import torchvision.transforms as tf
@@ -342,6 +348,164 @@ def pose_estimation_multi_view(batch, matcher: LoFTR):
 #                                       check_precond_strict=check_precond_strict)
 #     extrinsics, valid_batch = ba.run(kpts0_norm, kpts1_norm, confidence.squeeze(-1), init_T021)
 #     return extrinsics[:, 1], valid_batch
+
+################################################################
+# Weighted Perspective-N-Point
+# Adapted from kornia
+################################################################
+
+def _mean_isotropic_scale_normalize(points: torch.Tensor, eps: float = 1e-8) -> Tuple[torch.Tensor, torch.Tensor]:
+    r"""Normalizes points.
+
+    Args:
+       points : Tensor containing the points to be normalized with shape :math:`(B, N, D)`.
+       eps : Small value to avoid division by zero error.
+
+    Returns:
+       Tuple containing the normalized points in the shape :math:`(B, N, D)` and the transformation matrix
+       in the shape :math:`(B, D+1, D+1)`.
+    """
+    if not isinstance(points, torch.Tensor):
+        raise AssertionError(f"points is not an instance of torch.Tensor. Type of points is {type(points)}")
+
+    if len(points.shape) != 3:
+        raise AssertionError(f"points must be of shape (B, N, D). Got shape {points.shape}.")
+
+    x_mean = torch.mean(points, dim=1, keepdim=True)  # Bx1xD
+    scale = (points - x_mean).norm(dim=-1, p=2).mean(dim=-1)  # B
+
+    D_int = points.shape[-1]
+    D_float = torch.tensor(points.shape[-1], dtype=torch.float64, device=points.device)
+    scale = torch.sqrt(D_float) / (scale + eps)  # B
+    transform = eye_like(D_int + 1, points)  # (B, D+1, D+1)
+
+    idxs = arange(D_int, dtype=torch.int64, device=points.device)
+    transform[:, idxs, idxs] = transform[:, idxs, idxs] * scale[:, None]
+    transform[:, idxs, D_int] = transform[:, idxs, D_int] + (-scale[:, None] * x_mean[:, 0, idxs])
+
+    points_norm = transform_points(transform, points)  # BxNxD
+
+    return (points_norm, transform)
+
+
+def solve_pnp_dlt_weighted(
+    world_points: torch.Tensor,
+    img_points: torch.Tensor,
+    intrinsics: torch.Tensor,
+    weights: torch.Tensor,
+    svd_eps: float = 1e-4,
+)-> torch.Tensor:
+    """
+        Args:
+            world_points : A tensor with shape :math:`(B, N, 3)` representing
+            the points in the world space.
+            img_points : A tensor with shape :math:`(B, N, 2)` representing
+            the points in the image space.
+            intrinsics : A tensor with shape :math:`(B, 3, 3)` representing
+            the intrinsic matrices.
+            weights : A tensor with shape : math:'(B, N)' representing 
+            the 3D 2D matching confidence
+            svd_eps : A small float value to avoid numerical precision issues.
+
+        Returns:
+            A tensor with shape :math:`(B, 3, 4)` representing the estimated world to
+            camera transformation matrices (also known as the extrinsic matrices).
+    """
+    B, N = world_points.shape[:2]
+
+    # Getting normalized world points.
+    world_points_norm, world_transform_norm = _mean_isotropic_scale_normalize(world_points)
+
+    # Checking if world_points_norm (of any element of the batch) has rank = 3. This
+    # function cannot be used if all world points (of any element of the batch) lie
+    # on a line or if all world points (of any element of the batch) lie on a plane.
+    s = _torch_linalg_svdvals(world_points_norm)
+    if torch.any(s[:, -1] < svd_eps):
+        raise AssertionError(
+            "The last singular value of one/more of the elements of the batch is smaller "
+            f"than {svd_eps}. This function cannot be used if all world_points (of any "
+            "element of the batch) lie on a line or if all world_points (of any "
+            "element of the batch) lie on a plane."
+        )
+
+    intrinsics_inv = torch.inverse(intrinsics)
+    world_points_norm_h = convert_points_to_homogeneous(world_points_norm)
+
+    # Transforming img_points with intrinsics_inv to get img_points_inv
+    img_points_inv = transform_points(intrinsics_inv, img_points)
+
+    # Normalizing img_points_inv
+    img_points_norm, img_transform_norm = _mean_isotropic_scale_normalize(img_points_inv)
+    inv_img_transform_norm = torch.inverse(img_transform_norm)
+
+    # Setting up the system (the matrix A in Ax=0)
+    system = zeros((B, 2 * N, 12), dtype=world_points.dtype, device=world_points.device)
+    system[:, 0::2, 0:4] = world_points_norm_h
+    system[:, 1::2, 4:8] = world_points_norm_h
+    system[:, 0::2, 8:12] = world_points_norm_h * (-1) * img_points_norm[..., 0:1]
+    system[:, 1::2, 8:12] = world_points_norm_h * (-1) * img_points_norm[..., 1:2]
+
+    # apply the weights to the linear system
+    w_diag = torch.diag_embed(weights)
+    system = w_diag @ system
+
+    # Getting the solution vectors.
+    _, _, v = torch.svd(system)
+    solution = v[..., -1]
+
+    # Reshaping the solution vectors to the correct shape.
+    solution = solution.reshape(B, 3, 4)
+
+    # Creating solution_4x4
+    solution_4x4 = eye_like(4, solution)
+    solution_4x4[:, :3, :] = solution
+
+    # De-normalizing the solution
+    intermediate = torch.bmm(solution_4x4, world_transform_norm)
+    solution = torch.bmm(inv_img_transform_norm, intermediate[:, :3, :])
+
+    # We obtained one solution for each element of the batch. We may
+    # need to multiply each solution with a scalar. This is because
+    # if x is a solution to Ax=0, then cx is also a solution. We can
+    # find the required scalars by using the properties of
+    # rotation matrices. We do this in two parts:
+
+    # First, we fix the sign by making sure that the determinant of
+    # the all the rotation matrices are non-negative (since determinant
+    # of a rotation matrix should be 1).
+    det = torch.det(solution[:, :3, :3])
+    ones = ones_like(det)
+    sign_fix = where(det < 0, ones * -1, ones)
+    solution = solution * sign_fix[:, None, None]
+
+    # Then, we make sure that norm of the 0th columns of the rotation
+    # matrices are 1. Do note that the norm of any column of a rotation
+    # matrix should be 1. Here we use the 0th column to calculate norm_col.
+    # We then multiply solution with mul_factor.
+    norm_col = torch.norm(input=solution[:, :3, 0], p=2, dim=1)
+    mul_factor = (1 / norm_col)[:, None, None]
+    temp = solution * mul_factor
+
+    # To make sure that the rotation matrix would be orthogonal, we apply
+    # QR decomposition.
+    ortho, right = torch.linalg.qr(temp[:, :3, :3])
+
+    # We may need to fix the signs of the columns of the ortho matrix.
+    # If right[i, j, j] is negative, then we need to flip the signs of
+    # the column ortho[i, :, j]. The below code performs the necessary
+    # operations in a better way.
+    mask = eye_like(3, ortho)
+    col_sign_fix = torch.sign(mask * right)
+    rot_mat = torch.bmm(ortho, col_sign_fix)
+
+    # Preparing the final output.
+    pred_world_to_cam = torch.cat([rot_mat, temp[:, :3, 3:4]], dim=-1)
+
+    # TODO: Implement algorithm to refine the solution.
+
+    return pred_world_to_cam
+
+
 
 
 
